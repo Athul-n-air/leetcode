3627217bb25d504a90d2150054ed7ab39f7f741a
Athul Nair <152014556+Athul-n-air@users.noreply.github.com>
2026-09-19 12:53:58 +0530

    Time: 5 ms (53.71%) | Memory: 53.3 MB (91.94%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> grayCode(int n) {
+        ArrayList<Integer> ans  = new ArrayList<>();
         int size = 1<<n;
-        ArrayList<Integer> ans  = new ArrayList<>(size);
         for(int i =0;i<size;i++){
             int gray  = i^(i>>1);
             ans.add(gray);
