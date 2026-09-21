bc52682f05f76dd48145cfc37f36287a050e7b5a
Athul Nair <152014556+Athul-n-air@users.noreply.github.com>
2026-09-21 17:45:50 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (65.89%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,16 +14,20 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
-    }
-    public boolean helper(TreeNode root,long min, long max){
-        if(root==null){
+    public boolean isBst(TreeNode root, long min, long max){
+        if(root== null){
             return true;
         }
-        if(root.val<=min||root.val>=max){
+        if(root.val<=min || root.val>=max){
             return false;
         }
-        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+
+        return isBst(root.left,min, root.val) && isBst(root.right,root.val,max);
+    }
+    public boolean isValidBST(TreeNode root) {
+    long min = Long.MIN_VALUE;
+    long max = Long.MAX_VALUE;
+    return isBst(root,min,max);
+        
     }
 }
